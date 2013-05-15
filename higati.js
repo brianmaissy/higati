bfb43462a9df1c53ccd34cdf9207bce959d07a11
Brian Maissy <brian.maissy@gmail.com>
2013-05-15 22:19:12 +0300

    first version of higati is working

--- higati.js
+++ higati.js
@@ -1,19 +1,38 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to higati.";
-  };
+Numbers = new Meteor.Collection("numbers");
+var TOP_NUMBER = 38;
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+if (Meteor.isClient) {
+  Template.responses.numbers = function () {
+    return Numbers.find({}, {sort: {number: 1}});
+  };
+  Template.new_response.numbers = function () {
+    return Numbers.find({}, {sort: {number: 1}});
+  };
+  Template.new_response.events({
+    'click input#new-response-submit': function(event){
+      var numberBox = document.getElementById('new-response-number');
+      var number = parseInt(numberBox[numberBox.selectedIndex].value);
+      var id = Numbers.findOne({number: number})._id;
+      Numbers.update({_id: id}, {$set: {arrived: true}});
+    }
+  });
+  Template.reset.events({
+    'click input#reset': function(event){
+      for(var i=1; i<=TOP_NUMBER; i++){
+        var id = Numbers.findOne({number: i})._id;
+        Numbers.update({_id: id}, {$set: {arrived: false}});
+      }
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if(Numbers.find({}).count() == 0){
+      for(var i=1; i<=TOP_NUMBER; i++){
+        Numbers.insert({number: i, arrived: false});
+      }
+    }
   });
 }
+
